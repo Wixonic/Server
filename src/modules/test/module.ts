4d93718472bc74b31d9332771382f44b47b752ae
Wixonic <contact@wixonic.fr>
2026-04-20 11:18:41 +0200

    feat: Remove test module and add ERPC

--- src/modules/test/module.ts
+++
@@ -1,7 +0,0 @@
-import { config } from "../../config.ts";
-import type { Handler } from "../../main.ts";
-
-export const handler: Handler = {
-	domain: config.isDevEnvironment ? "localhost:1200" : "server.wixonic.fr",
-	handle: () => new Response("Hi!")
-};
